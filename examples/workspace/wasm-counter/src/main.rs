a85a803921361ea4af40bbc1f71270ce34d0d8ec
Florian <florian@uekermann.me>
2023-06-26 17:21:54 +0200

    add workspace example and update wasm-bindgen

---
+++ examples/workspace/wasm-counter/src/main.rs
@@ -0,0 +1,55 @@
+use gloo::timers::future::sleep;
+use std::rc::Rc;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::time::Duration;
+use wasm_bindgen::{closure::Closure, JsCast};
+use web_sys::{HtmlButtonElement, HtmlParagraphElement};
+
+fn main() {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    wasm_logger::init(wasm_logger::Config::new(log::Level::Info));
+    wasm_bindgen_futures::spawn_local(run())
+}
+
+static COUNTER: AtomicUsize = AtomicUsize::new(0);
+
+async fn run() {
+    log::info!("start");
+
+    let document = web_sys::window().unwrap().document().unwrap();
+    let button: HtmlButtonElement = document
+        .create_element("button")
+        .unwrap()
+        .dyn_into()
+        .unwrap();
+    let p: HtmlParagraphElement = document.create_element("p").unwrap().dyn_into().unwrap();
+    document.body().unwrap().append_child(&p).unwrap();
+    document.body().unwrap().append_child(&button).unwrap();
+
+    button.set_text_content(Some("+1"));
+    inc(&p);
+
+    let rc_p = Rc::new(p.clone());
+    let cb = move || {
+        inc(&rc_p);
+    };
+    let cb = Closure::wrap(Box::new(cb) as Box<dyn FnMut()>);
+    button
+        .add_event_listener_with_callback("click", cb.as_ref().unchecked_ref())
+        .unwrap();
+    cb.forget();
+
+    for _ in 0..3 {
+        sleep(Duration::from_secs(5)).await;
+        log::info!("auto-inc");
+        inc(&p);
+    }
+}
+
+fn inc(p: &HtmlParagraphElement) {
+    log::info!("inc");
+    let old = COUNTER.load(Ordering::SeqCst);
+    let new = library::add(old, 1);
+    COUNTER.store(new, Ordering::SeqCst);
+    p.set_text_content(Some(&format!("{}", old)));
+}
--- src/test.rs
+++ src/test.rs
@@ -2,8 +2,12 @@ use std::{env::var, fs, path::Path, process::Command};
 
 use crate::{exec, run, Opt};
 
-fn bundle_example(crate_name: &str, opt: Opt) {
-    let base_dir = Path::new("examples").join(crate_name);
+fn bundle_example(workspace: Option<&str>, crate_name: &str, opt: Opt) {
+    let mut base_dir = Path::new("examples").to_path_buf();
+    if let Some(workspace) = workspace {
+        base_dir = base_dir.join(workspace);
+    }
+    base_dir = base_dir.join(crate_name);
     cargo_clean(&base_dir);
     exec(&base_dir, &opt);
     let mut html_path = base_dir.join("target/wasm-bundle/debug");
@@ -23,17 +27,22 @@ fn cargo_clean(base_dir: &Path) {
 
 #[test]
 fn wasm_counter() {
-    bundle_example("wasm-counter", Opt::default())
+    bundle_example(None, "wasm-counter", Opt::default())
 }
 
 #[test]
 fn yew_counter() {
-    bundle_example("yew-counter", Opt::default())
+    bundle_example(None, "yew-counter", Opt::default())
 }
 
 #[test]
 fn library() {
     let mut opt = Opt::default();
     opt.example = Some("demo".to_string());
-    bundle_example("library", opt)
+    bundle_example(None, "library", opt)
+}
+
+#[test]
+fn workspace() {
+    bundle_example(Some("workspace"), "wasm-counter", Opt::default())
 }
